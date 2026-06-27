fe80d9b23b1e77e17ab91f8df8d8c7a101f966cc
akoele <externe.akoele.akoesso-epse-gbenou@allianz.fr>
2026-06-27 22:19:49 +0400

    Add Splunk JFrog repository for splunk-library-javalogging dependency

--- starter_code/src/main/java/com/example/demo/SareetaApplication.java
+++ starter_code/src/main/java/com/example/demo/SareetaApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -8,11 +9,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories("com.example.demo.model.persistence.repositories")
 @EntityScan("com.example.demo.model.persistence")
+@Slf4j
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SareetaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SareetaApplication.class, args);
+        log.info("Test splunk");
 	}
 
 }
